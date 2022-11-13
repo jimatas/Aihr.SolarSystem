dfe35309416b630c0377b8de459c9536c3204ef8
jimatas <atask@hr.nl>
2022-11-13 15:55:55 +0100

    Colonization extension methods now act upon the base SolarSystemObject type.

--- Aihr.SolarSystem/Extensions/ColonizationExtensions.cs
+++ Aihr.SolarSystem/Extensions/ColonizationExtensions.cs
@@ -3,65 +3,35 @@
 namespace Aihr.SolarSystem.Extensions
 {
     /// <summary>
-    /// Per the assignment, provides extension methods for colonizing candidate planets and moons.
+    /// Per the assignment, provides extension methods for colonizing celestial bodies, with planets and moons being potential candidates.
     /// </summary>
     public static class ColonizationExtensions
     {
         /// <summary>
-        /// Checks to see if a planet can be colonized.
+        /// Checks to see if a celestial body can be colonized.
         /// </summary>
         /// <param name="candidate"></param>
         /// <returns><c>true</c> or <c>false</c> as <paramref name="candidate"/> can either be colonized or not.</returns>
-        public static bool CanBeColonized(this Planet candidate)
+        public static bool CanBeColonized(this SolarSystemObject candidate)
         {
             return (candidate is IPossiblyLifeSustaining possiblyLifeSustaining && possiblyLifeSustaining.CanSustainLife)
                 || (candidate is IPossiblyTerraformable possiblyTerraformable && possiblyTerraformable.IsTerraformable);
         }
 
         /// <summary>
-        /// Performs the actual act of colonizing a planet.
+        /// Performs the actual act of colonizing the celestial body.
         /// </summary>
         /// <param name="candidate"></param>
         /// <exception cref="ColonizationNotPossibleException">If <paramref name="candidate"/> is not fit for colonization. 
-        /// Determine that first using <see cref="CanBeColonized(Planet)"/>.</exception>
-        public static void Colonize(this Planet candidate)
+        /// Determine that first using the <see cref="CanBeColonized(SolarSystemObject)"/> method.</exception>
+        public static void Colonize(this SolarSystemObject candidate)
         {
             if (!candidate.CanBeColonized())
             {
-                ThrowColonizationNotPossibleExceptionWithDefaultMessage(candidate);
+                throw new ColonizationNotPossibleException($"Colonization of {candidate.Name} is not possible; it neither sustains life naturally, nor can it be terraformed.");
             }
 
             // TODO: Colonize
         }
-
-        /// <summary>
-        /// Checks to see if a moon can be colonized.
-        /// </summary>
-        /// <param name="candidate"></param>
-        /// <returns><c>true</c> or <c>false</c> as <paramref name="candidate"/> can either be colonized or not.</returns>
-        public static bool CanBeColonized(this Moon candidate)
-        {
-            return (candidate is IPossiblyLifeSustaining possiblyLifeSustaining && possiblyLifeSustaining.CanSustainLife)
-                || (candidate is IPossiblyTerraformable possiblyTerraformable && possiblyTerraformable.IsTerraformable);
-        }
-
-        /// <summary>
-        /// Performs the actual act of colonizing a moon.
-        /// </summary>
-        /// <param name="candidate"></param>
-        /// <exception cref="ColonizationNotPossibleException">If <paramref name="candidate"/> is not fit for colonization. 
-        /// Determine that first using <see cref="CanBeColonized(Moon)"/>.</exception>
-        public static void Colonize(this Moon candidate)
-        {
-            if (!candidate.CanBeColonized())
-            {
-                ThrowColonizationNotPossibleExceptionWithDefaultMessage(candidate);
-            }
-
-            // TODO: Colonize
-        }
-
-        private static string ThrowColonizationNotPossibleExceptionWithDefaultMessage(SolarSystemObject candidate)
-            => throw new ColonizationNotPossibleException($"Colonization of {candidate.Name} is not possible; it neither sustains life naturally, nor can it be terraformed.");
     }
 }
